741be7d3b31cd7d47996f0552f7ba4ee220245d6
Moosabilal <moosabilal75608@gmail.com>
2024-12-20 14:17:40 +0530

    stop tracking .env file

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express')
-const app = express()
+const app = express();
+const path = require('path');
 const env=require('dotenv').config();
+const userRouter=require('./routes/userRouter')
 const db = require('./config/db')
 db()
 
-app.listen(process.env.PORT,()=>{
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.set('view engine','ejs');
+app.set('views',[path.join(__dirname,'views/user'),path.join(__dirname,'views/admin')]);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/',userRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log( `Server is started running `);
 })
 
