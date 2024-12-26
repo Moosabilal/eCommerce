5a54b1d70b5a99beacac8744e31b6e2f85d17643
Moosabilal <moosabilal75608@gmail.com>
2024-12-26 20:45:12 +0530

    category list and unlist

--- controllers/admin/categoryController.js
+++ controllers/admin/categoryController.js
@@ -102,10 +102,33 @@ const removeCategoryOffer = async (req,res)=>{
     }
 }
 
+const getlistCategory = async (req,res)=>{
+    try {
+        let id = req.query.id;
+        await Category.updateOne({_id:id},{$set:{isListed:false}});
+        res.redirect("/admin/category")
+    } catch (error) {
+        res.redirect("/pageerror")
+    }
+}
+
+const getunlistCategory = async (req,res)=>{
+    try {
+        let id = req.query.id;
+        await Category.updateOne({_id:id},{$set:{isListed:true}});
+        res.redirect("/admin/category")
+    } catch (error) {
+        res.redirect("/pageerror")
+        
+    }
+}
+
 
 module.exports = {
     categoryInfo,
     addCategory,
     addCategoryOffer,
     removeCategoryOffer,
+    getlistCategory,
+    getunlistCategory,
 }
