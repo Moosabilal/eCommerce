3a12d85ab9e13c3d3865c8a42f19900b3f3e79fe
Moosabilal <moosabilal75608@gmail.com>
2025-02-10 23:43:17 +0530

    last commit3

--- app.js
+++ app.js
@@ -52,8 +52,16 @@ app.use('/admin',adminRouter);
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log( PORT, `Server is started running `);
-})
+// app.listen(PORT, '0.0.0.0', () => {
+//     console.log( PORT, `Server is started running `);
+// })
+app.listen(PORT, '0.0.0.0', (err) => {
+    if (err) {
+      console.error(`Error starting server: ${err}`);
+    } else {
+      console.log(`Server is running on port ${PORT}`);
+    }
+  });
+  
 
 module.exports = app;
