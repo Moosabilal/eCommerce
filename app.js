e76688b997ba40a190ea36cf4e222037f95fb0d9
Moosabilal <moosabilal75608@gmail.com>
2025-02-10 23:36:47 +0530

    last commit2

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use('/admin',adminRouter);
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log( PORT, `Server is started running `);
 })
 
