0731448aa4c627bfae1daef187f8827c2206e1c0
Moosabilal <moosabilal75608@gmail.com>
2026-05-10 10:54:41 +0530

    fix:/ fixed the 401 production error by incrementing the limit

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ db()
 const app = express();
 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
